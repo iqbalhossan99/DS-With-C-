050a5f73fa85a4180cd6bcec3f758f249a8baa44
MD IQBAL HOSSAN <engiqbal1999@gmail.com>
2023-02-02 22:21:00 +0600

    insertAtAnyWhere, DeleteAtAnyWhere in build-in linked-list

--- week-6/ml-22-build-in-linked-list-&-stack/build-in-linked-list.cpp
+++ week-6/ml-22-build-in-linked-list-&-stack/build-in-linked-list.cpp
@@ -1,16 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print(list<int>dl)
+void print(list<int>&dl)
 {
     //  o(n)
-    //    list<int>::iterator a = dl.begin();
-    auto a = dl.begin();
-    while(a != dl.end())
+    list<int>::iterator a = dl.begin();
+
+    while( a != dl.end())
     {
-        cout<<*a <<" ";
+        cout << *a <<" ";
         a++;
     }
+}
+
+void InsertAtAnyWhere(list<int>&dl, int index, int value)
+{
+    auto it = dl.begin(); //o(1)
+    advance(it, index); // it will move one index right of the item o(index)
+    dl.insert(it, value); //O(1)
+}
+
+void DeleteAtAnyWhere(list<int>&dl, int index)
+{
+    auto it = dl.begin();
+    advance(it, index);
+    dl.erase(it);
+
 }
 
 int main()
@@ -28,12 +43,26 @@ int main()
     //print(l);
 
     // pop_back() o(1)
-    l.pop_back();
+    //l.pop_back();
     //print(l);
 
     // pop_front() o(1)
-    l.pop_front();
+   // l.pop_front();
+    //print(l);
+
+    //insert at any where
+    InsertAtAnyWhere(l, 3, 100);
     print(l);
+
+    cout<<"\n";
+
+    //Delete at any where
+    DeleteAtAnyWhere(l, 2);
+    print(l);
+    cout<<"\n";
+    // size
+    cout<<l.size();
+
 
      return 0;
 }

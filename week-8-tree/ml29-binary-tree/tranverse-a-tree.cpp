16df88320ea93235bce39da4f53be17f3c30bb79
MD IQBAL HOSSAN <engiqbal1999@gmail.com>
2023-02-14 08:43:42 +0600

    tree traverse using BFS

---
+++ week-8-tree/ml29-binary-tree/tranverse-a-tree.cpp
@@ -0,0 +1,121 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int id;
+    node *Left;
+    node *Right;
+    node *Parent;
+
+};
+
+class Binary_Tree
+{
+public:
+    node* root;
+
+
+    BinaryTree()
+    {
+        root = NULL;
+    }
+
+    node *CreateNewNode(int id)
+    {
+        node *newnode = new node;
+        newnode -> id = id;
+        newnode ->Left = NULL;
+        newnode ->Right = NULL;
+        newnode ->Parent = NULL;
+
+        return newnode;
+    }
+
+    void build_binary_tree()
+    {
+                node* allnode[6];
+
+        for(int i=0; i<6; i++)
+        {
+            allnode[i] = CreateNewNode(i);
+        }
+
+        allnode[0]->Left = allnode[1];
+        allnode[0]->Right = allnode[2];
+
+        allnode[1]->Left = allnode[3];
+        allnode[1]->Right = allnode[4];
+        allnode[1]->Parent = allnode[0];
+
+        allnode[2]->Right = allnode[5];
+        allnode[2]->Parent = allnode[0];
+
+        allnode[3]->Parent = allnode[1];
+        allnode[4]->Parent = allnode[1];
+
+        root = allnode[0];
+    }
+
+//    void print_tree()
+//    {
+//        for(int i=0; i<6; i++)
+//        {
+//            int p = -1;
+//            int l = -1;
+//            int r = -1;
+//
+//            if(allnode[i]->Parent != NULL)
+//                p = allnode[i]->Parent->id;
+//
+//            if(allnode[i]->Left != NULL)
+//                l = allnode[i]->Left->id;
+//
+//            if(allnode[i]->Right != NULL)
+//                r = allnode[i]->Right->id;
+//
+//            cout<<"Node "<<i<<": Parent = "<<p<<" , Left child = "<<l<<" , Right child = "<<r<<"\n";
+//
+//        }
+//    }
+
+                void BFS(){
+                                queue<node*>q;
+                                q.push(root);
+
+                                while(!q.empty())
+                                {
+                                          node* a = q.front();
+                                          q.pop();
+                                          int p = -1, l = -1, r = -1;
+                                          if(a->Left != NULL)
+                                          {
+                                                    l = a->Left->id;
+                                                    q.push(a->Left);
+                                          }
+                                          if(a->Right != NULL)
+                                          {
+                                                    r = a->Right->id;
+                                                    q.push(a->Right);
+                                          }
+                                          if(a->Parent != NULL)
+                                          {
+                                                    p = a->Parent->id;
+                                          }
+
+                                          cout<<"Node id = "<<a->id<<" Left Child = "<<l;
+                                          cout<<" Right Child = "<<r<<" Parent Id = "<<p<<"\n";
+                                }
+                }
+};
+
+int main()
+{
+    Binary_Tree bt;
+    bt.build_binary_tree();
+    bt.BFS();
+    return 0;
+}
+

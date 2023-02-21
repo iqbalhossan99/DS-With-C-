8c37b35998a32934aea4c87390afd59ee9ead77f
MD IQBAL HOSSAN <engiqbal1999@gmail.com>
2023-02-21 19:35:11 +0600

    getMax and getExtractMax from a heap

--- week-9-heap/ml-32-heap/max-heap.cpp
+++ week-9-heap/ml-32-heap/max-heap.cpp
@@ -1,8 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class MaxHeap
-{
+class MaxHeap{
 public:
     vector<int>nodes;
 
@@ -11,75 +10,105 @@ public:
 
     }
 
-    //O(n)
-    void UpHeapify(int idx)
+    //O(logn)
+    void up_heapify(int idx)
     {
-        while(idx>0 && nodes[idx] > nodes[(idx-1)/2])
+        while(idx > 0 && nodes[idx] > nodes[(idx-1)/2])
         {
-            swap(nodes[idx], nodes[(idx-1)/2]);
+            swap(nodes[idx] , nodes[(idx-1)/2]);
             idx = (idx-1)/2;
         }
     }
-
-    // Insert O(logn)
-    void Insert(int value)
+    //O(logn)
+    void insert(int x)
     {
-        nodes.push_back(value);
-        UpHeapify(nodes.size()-1);
+        nodes.push_back(x);
+        up_heapify(nodes.size()-1);
     }
 
-    // Down Heapify O(logn)
-    void DownHeapify(int idx)
-    {
-                    while(1)
-                    {
-                                    int largest = idx;
-                                    int l = 2*idx + 1;
-                                    int r = 2*idx + 2;
-                                    if(l<nodes.size() && nodes[largest] < nodes[l])
-                                                largest = l;
-                                    if(r<nodes.size() && nodes[largest] < nodes[r])
-                                                largest = r;
-                                    if(largest == idx)
-                                                return;
-
-                                    swap(nodes[idx], nodes[largest]);
-                                    idx = largest;
-
-                    }
-    }
-
-    //Delete
-    void Delete(int idx)
-    {
-        swap(nodes[idx], nodes[nodes.size()-1]);
-        nodes.pop_back();
-        DownHeapify(idx);
-    }
-
+    //O(n)
     void PrintHeap()
     {
-        for(int i=0; i<nodes.size(); i++)
+        for(int i=0;i<nodes.size();i++)
         {
             cout<<nodes[i]<<" ";
         }
+        cout<<"\n";
+    }
+
+    //O(logn)
+    void down_heapify(int idx)
+    {
+        while(1)
+        {
+            int largest = idx;
+            int l = 2*idx + 1;
+            int r = 2*idx + 2;
+            if(l<nodes.size() && nodes[largest] < nodes[l])
+            {
+                largest = l;
+            }
+            if(r<nodes.size() && nodes[largest] < nodes[r])
+            {
+                largest = r;
+            }
+            if(largest==idx)
+                break;
+            swap(nodes[idx] , nodes[largest]);
+            idx = largest;
+        }
+
+    }
+
+    //O(logn)
+    void Delete(int idx)
+    {
+        if(idx >= nodes.size())
+            return;
+        swap(nodes[idx] , nodes[nodes.size()-1]);
+        nodes.pop_back();
+        down_heapify(idx);
+    }
+
+    //O(1)
+    int GetMax()
+    {
+        if(nodes.empty())
+        {
+            cout<<"Heap is empty!\n";
+            return -1;
+        }
+        return nodes[0];
+    }
+
+    //O(logn)
+    int ExtractMax()
+    {
+        if(nodes.empty())
+        {
+            cout<<"Heap is empty!\n";
+            return -1;
+        }
+        int ret = nodes[0];
+        Delete(0);
+        return ret;
     }
 };
-
 
 int main()
 {
     MaxHeap heap;
-    heap.Insert(4);
-    heap.Insert(7);
-    heap.Insert(9);
-    heap.Insert(1);
-    heap.Insert(10);
-    heap.Insert(20);
-    heap.Insert(30);
-    heap.PrintHeap();
-    heap.Delete(0);
-    cout<<"\n";
-    heap.PrintHeap();
+    heap.insert(4);
+    heap.insert(7);
+    heap.insert(9);
+    heap.insert(1);
+    heap.insert(10);
+    heap.insert(20);
+    heap.insert(30);
+    cout<<"Max element = "<<heap.GetMax()<<"\n";
+//    heap.PrintHeap();
+
+    cout<<"Max element = "<<heap.ExtractMax()<<"\n";
+    cout<<"Max element = "<<heap.ExtractMax()<<"\n";
     return 0;
 }
